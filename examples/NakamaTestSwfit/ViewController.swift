994cd8c11d145564c9ff8097991fce3538913e71
Allan Nava <allannava95@gmail.com>
2021-04-07 19:35:26 +0200

    added example project

---
+++ examples/NakamaTestSwfit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NakamaTestSwfit
+//
+//  Created by Allan Nava on 07/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
