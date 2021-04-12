14cc0b92582ff5c43c9a4e1eebad9e9a413ed7d5
allan <allan.nava@ies-italia.it>
2021-04-12 10:49:14 +0200

    fix nakama tests

--- Tests/NakamaTests/NakamaTests.swift
+++ Tests/NakamaTests/NakamaTests.swift
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-import XCTest
-import Foundation
-
-#if !SWIFT_PACKAGE
-    import PromiseKit
-#endif
-
-#if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
-    public protocol XCTestCaseProvider {
-        var allTests: [(String, () throws -> Void)] { get }
-    }
-#endif
-
-class NakamaTests: XCTestCase, XCTestCaseProvider {
-    var allTests: [(String, () throws -> Void)] {
-        return [
-            ("testRest", testRest),
-        ]
-    }
-
-    #if SWIFT_PACKAGE
-
-    func testRest() {
-        let start = NSDate()
-        let end = NSDate()
-        let duration = end.timeIntervalSince(start as Date)
-        XCTAssertGreaterThanOrEqual(duration, 0)
-        XCTAssertLessThan(duration, 2)
-    }
-
-    #else
-
-    // Xcode Only: SPM doesn't seem to support building testDependencies
-    //             and PromiseKit doesn't support SPM.
-    func testRest() {
-        let start = NSDate()
-        let promise = rest()
-        let end = NSDate()
-        let duration = end.timeIntervalSinceDate(start)
-        expect(duration) >= 0
-        expect(duration) < 2
-
-        var promisedInterval: NSTimeInterval?
-        waitUntil(timeout: 5) { done in
-            promise.then { (interval) -> NSTimeInterval in
-                promisedInterval = interval
-                return interval
-            }.always(done)
-        }
-
-        expect(promisedInterval) >= 0
-        expect(promisedInterval) < 2
-    }
-    #endif
-=======
 /*
  * Copyright 2021 The Nakama Authors
  *
@@ -147,5 +91,4 @@ final class NakamaTests: XCTestCase {
         socket2.disconnect()
         try! client.disconnect().wait()
     }
->>>>>>> c60ac9930aee80c93f39e60498454cc4793c75b2
 }
