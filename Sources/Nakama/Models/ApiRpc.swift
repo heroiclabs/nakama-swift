6fedb08e53e8e3382394a64ebe1d1a48e4d71947
Fayyad <fayyadsufyan@gmail.com>
2023-08-31 20:35:10 +0400

    Add support for RPC calls from client.

---
+++ Sources/Nakama/Models/ApiRpc.swift
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2023 Heroic Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Foundation
+
+public struct ApiRpc {
+    var id: String
+    var payload: String
+    var httpKey: String
+    
+    init(id: String, payload: String, httpKey: String) {
+        self.id = id
+        self.payload = payload
+        self.httpKey = httpKey
+    }
+}
