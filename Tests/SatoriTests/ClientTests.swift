ae8a54cbdb637b37d75e2b147803c7b9a16ca210
Fayyad <fayyadsufyan@gmail.com>
2024-03-09 14:04:14 +0400

    Add http client tests.

--- Tests/SatoriTests/ClientTests.swift
+++ Tests/SatoriTests/ClientTests.swift
@@ -15,12 +15,60 @@
  */
 
 import XCTest
+@testable
 import Satori
 
 final class ClientTests: XCTestCase {
-    var session: SatoriSession!
-    
+    private let client = HttpClient(host: "127.0.0.1", port: 7450, apiKey: "apiKey")
+
     override func setUp() async throws {
         
+    }
+    
+    override func tearDown() async throws {
+        
+    }
+
+    public func test_AuthenticateAndLogout() async throws {
+        let session = try await client.authenticate(id: "285fb548-1c23-42c2-84b5-cd18c22d7053")
+        try await client.authenticateLogout(session: session)
+        do {
+            let _ = try await client.getExperiments(session: session, names: [])
+            XCTAssert(false, "Session should be invalidated")
+        } catch {
+            XCTAssert(true)
+        }
+    }
+
+    public func test_GetExperiments() async throws {
+        let session = try await client.authenticate(id: "285fb548-1c23-42c2-84b5-cd18c22d7053")
+        let experiments = try await client.getAllExperiments(session: session)
+        
+        XCTAssertTrue(experiments.experiments?.count == 1)
+    }
+
+    public func test_GetFlags() async throws {
+        let session = try await client.authenticate(id: "285fb548-1c23-42c2-84b5-cd18c22d7053")
+        let flags = try await client.getFlags(session: session, names: [])
+        XCTAssertTrue(flags.flags?.count == 4)
+        
+        let namedFlags = try await client.getFlags(session: session, names: ["Min-Build-Number"])
+        XCTAssertTrue(namedFlags.flags?.count == 1)
+    }
+
+    public func test_SendEvents() async throws {
+        let session = try await client.authenticate(id: "285fb548-1c23-42c2-84b5-cd18c22d7053")
+        try await client.event(session: session, event: Event(name: "gameFinished", timestamp: Date(), metadata: ["score":"1000"]))
+        try await client.events(session: session, events: [
+            Event(name: "adStarted", timestamp: Date()),
+            Event(name: "appLaunched", timestamp: Date())
+        ])
+    }
+
+    public func test_GetLiveEvent() async throws {
+        let session = try await client.authenticate(id: "285fb548-1c23-42c2-84b5-cd18c22d7053")
+        let liveEvents = try await client.getLiveEvents(session: session)
+        
+        XCTAssertTrue(liveEvents.liveEvents == nil)
     }
 }
