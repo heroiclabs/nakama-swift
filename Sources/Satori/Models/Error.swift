8dbe9381917c6807fd4cda6591d12a4cd383d505
Fayyad <fayyadsufyan@gmail.com>
2024-03-14 21:30:38 +0400

    Fix indentation.

--- Sources/Satori/Models/Error.swift
+++ Sources/Satori/Models/Error.swift
@@ -16,7 +16,7 @@
 
 /// Custom error type for Satori.
 enum SatoriError: Error {
-	/// No matching flag found.
-	case noMatchingFlag
+    /// No matching flag found.
+    case noMatchingFlag
     case invalidURL
 }
